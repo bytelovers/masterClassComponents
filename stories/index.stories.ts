ae7530128cab331eacdae43461b9d8c48070ac2b
Alfredo de la Calle <alfredo.delacalle@gmail.com>
2018-03-26 08:49:45 +0200

    [Del] auto-import encapsulation file

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -3,8 +3,6 @@ import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import '../stories/encapsulation/encapsulation.stories';
-
 import { Welcome } from '@storybook/angular/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => ({
