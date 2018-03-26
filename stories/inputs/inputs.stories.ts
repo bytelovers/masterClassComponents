122645db853149c371131ace342de6a73e92cc48
Alfredo de la Calle <alfredo.delacalle@gmail.com>
2018-03-26 10:41:42 +0200

    [upd] input stateful component

--- stories/inputs/inputs.stories.ts
+++ stories/inputs/inputs.stories.ts
@@ -3,17 +3,17 @@ import { withKnobs, text } from '@storybook/addon-knobs/angular';
 
 import { DefaultInputModule } from "./input.module";
 
-import { DefaultComponent } from './default-input/default.component';
+import { StatefulComponent } from './default-input/stateful.component';
 
 storiesOf('Inputs', module)
     .addDecorator(
         moduleMetadata({
-        imports: [DefaultInputModule]
+            imports: [DefaultInputModule]
         })
     )
     .addDecorator(withKnobs)
     .add('Default', () => ({
-        component: DefaultComponent,
+        component: StatefulComponent,
         props: {
             title: text('title', 'test title')
         }
